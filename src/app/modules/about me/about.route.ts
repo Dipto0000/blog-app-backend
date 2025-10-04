53d08d1704624d8c3d2c0db6d3420900442fbe1d
Dipto0000 <dipto2041@gmail.com>
2025-10-04 13:31:51 +0600

    routes protected

--- src/app/modules/about me/about.route.ts
+++ src/app/modules/about me/about.route.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { AboutController } from "./about.controller";
+import { checkAuth } from "../../middleswares/checkAuth";
+import { Role } from "../user/user.interface";
 
 
 const router = Router();
 
-router.post("/create", AboutController.createAboutInfo);
+router.post("/create", checkAuth(Role.SuperAdmin), AboutController.createAboutInfo);
+router.get("/", AboutController.getAboutInfo);
 
 
 
--- src/app/modules/projects/projects.route.ts
+++ src/app/modules/projects/projects.route.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { ProjectController } from "./projects.controller";
+import { checkAuth } from "../../middleswares/checkAuth";
+import { Role } from "../user/user.interface";
 
 
 const router = Router();
 
-router.post("create", ProjectController.createProjectInfo);
+router.post("/create", checkAuth(Role.SuperAdmin), ProjectController.createProjectInfo);
 router.get("/", ProjectController.getProjectInfo);
-
 
 
 export const projectRoutes = router;
