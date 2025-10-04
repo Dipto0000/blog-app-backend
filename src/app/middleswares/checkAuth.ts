1f34aad7f85e40a8311e4d03da2404bccc76a68b
Dipto0000 <dipto2041@gmail.com>
2025-10-04 13:02:25 +0600

    auth errors fixed

--- src/app/middleswares/checkAuth.ts
+++ src/app/middleswares/checkAuth.ts
@@ -1,40 +1,38 @@
 import { NextFunction, Request, Response } from "express";
-import { IUser } from "../modules/user/user.interface";
 import { verifyToken } from "../utils/jwt";
 import { envVars } from "../config/env";
-import { JwtPayload } from "jsonwebtoken";
+import { JwtPayload, TokenExpiredError } from "jsonwebtoken";
 import { User } from "../modules/user/user.model";
 
 
-export const checkAuth = (...authRoles: string[]) => async(req: Request, res: Response, next: NextFunction) => {
-
+export const checkAuth = (...authRoles: string[]) => async (req: Request, res: Response, next: NextFunction) => {
     try {
-        
-        const accessToken = req.headers.authorization || req.cookies.accessToken;
-
-        if(!accessToken) {
-            return res.status(401).json({ message: "Unauthorized Access" });
-        }
-
-        const verifiedToken = verifyToken(accessToken, envVars.JWT_ACCESS_SECRET) as JwtPayload;
-
-        const isUserExist = await User.findOne({ email: verifiedToken.email });
-
-        if(!isUserExist) {
-            return res.status(401).json({ message: "Unauthorized User" });
-        }
-        
-        if(!authRoles.includes(isUserExist.role)){
-            throw new Error("You are not authorized to access this route");
-        }
-
-        req.user = verifiedToken;
-
-        next();
-
-    } catch (error) {
-        console.log(error);
-        next(error);
+      let accessToken = req.headers.authorization || req.cookies.accessToken;
+  
+      if (!accessToken) {
+        return res.status(401).json({ message: "Unauthorized Access: No token provided" });
+      }
+  
+      if (accessToken.startsWith("Bearer ")) {
+        accessToken = accessToken.split(" ")[1];
+      }
+  
+      const verifiedToken = verifyToken(accessToken, envVars.JWT_ACCESS_SECRET) as JwtPayload;
+  
+      const isUserExist = await User.findOne({ email: verifiedToken.email });
+      if (!isUserExist) {
+        return res.status(401).json({ message: "Unauthorized User" });
+      }
+  
+      if (authRoles.length && !authRoles.includes(isUserExist.role)) {
+        return res.status(403).json({ message: "Forbidden: Not enough privileges" });
+      }
+  
+      req.user = verifiedToken;
+      next();
+    } catch (error: any) {
+      console.error("Auth Error:", error.message);
+      return res.status(401).json({ message: "Invalid or expired token" });
     }
-
-}
+  };
+  
